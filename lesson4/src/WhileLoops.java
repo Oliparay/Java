10bfdc70e57109e8de090600d951162a5dc3d9db
Vadik <cmerch.9797@mail.ru>
2023-06-03 17:57:00 +0300

    урок 5

--- lesson4/src/WhileLoops.java
+++ lesson4/src/WhileLoops.java
@@ -4,9 +4,10 @@ public class WhileLoops {
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        int value = 10;
-        while(value>5){
-            System.out.println("Hello");
+        int value = 0;
+        while(value<5){
+            System.out.println("Hello" + value);
+            value = value+1;
 
         }
     }
---
+++ lesson5/src/ForLoop.java
@@ -0,0 +1,6 @@
+public class ForLoop {
+    public static void main(String[] args) throws Exception {
+        for(int i = 10;i>=00;i = i-5)
+        System.out.println("Hello"+i);       // i++ == i = i+i
+    }
+}
