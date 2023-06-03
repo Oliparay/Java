db9941f44462987f1cb0d1423bfcbc42839b81f0
Vadik <cmerch.9797@mail.ru>
2023-06-03 17:07:30 +0300

    мои уроки

---
+++ lesson3/src/App.java
@@ -0,0 +1,13 @@
+import javax.xml.transform.Source;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x = 5;
+        String s = "Hello";
+        String spase = " ";
+        String name = "Bob";
+        System.out.println(s+spase+name);
+        
+        System.out.println("My number is" + x);
+    }
+}
