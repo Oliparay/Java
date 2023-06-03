1ec1b73b6ee4d943a56b023c1d22e97864a230bd
Vadik <cmerch.9797@mail.ru>
2023-06-03 17:38:14 +0300

    урок 4

---
+++ lesson4/src/WhileLoops.java
@@ -0,0 +1,13 @@
+public class WhileLoops {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        int value = 10;
+        while(value>5){
+            System.out.println("Hello");
+
+        }
+    }
+}
